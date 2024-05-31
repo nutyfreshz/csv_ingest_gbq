60f972ba8c153460a8118e7135275aa55711aab9
nutyfreshz <98257732+nutyfreshz@users.noreply.github.com>
2024-05-31 10:25:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,10 @@
 import subprocess
 command = ["pip", "install", "-r", "requirements.txt"]
 
-file_id = '1zolNE97MtNtclIXFMvkmjzJVpgyonpKn'
-json_file_path = 'cdg-mark-cust-prd_customer_team.json'
-subprocess.run(['gdown', '--id', file_id, '-O', json_file_path], check=True)
-
 import streamlit as st
 import pandas as pd
 import pandas_gbq
 import json
-
-# Read the JSON file
-with open(json_file_path, 'r') as file:
-    json_data = json.load(file)
 
 # Title
 st.title("Upload CSV into GBQ App")
@@ -20,6 +12,9 @@ st.title("Upload CSV into GBQ App")
 # Upload CSV file
 st.sidebar.header("Upload Data")
 uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
+
+st.sidebar.header("Upload JSON Credential")
+uploaded_file_json = st.sidebar.file_uploader("Upload a JSON file", type=["json"])
 
 if uploaded_file is not None:
     @st.cache_data
