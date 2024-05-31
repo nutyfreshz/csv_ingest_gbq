66c87a54b2203bdb18344ef7253f1bf9cf44860b
nutyfreshz <98257732+nutyfreshz@users.noreply.github.com>
2024-05-31 10:09:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,22 @@ command = ["pip", "install", "-r", "requirements.txt"]
 import streamlit as st
 import pandas as pd
 import pandas_gbq
+
+# Title
+st.title("Upload CSV into GBQ App")
+
+# Upload CSV file
+st.sidebar.header("Upload Data")
+uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
+
+if uploaded_file is not None:
+    @st.cache
+    def load_data():
+        data = pd.read_csv(uploaded_file)
+        return data
+
+    data = load_data()
+    st.sidebar.markdown("### Data Sample")
+    st.sidebar.write(data.head())
+else:
+    st.warning("Please upload a CSV file.")
